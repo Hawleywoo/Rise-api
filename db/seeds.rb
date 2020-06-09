972150b21485a6c017d0d2bf15a28f0508fa1474
matthewmjm <matthewmjm@yahoo.com>
2020-06-09 12:10:15 -0600

    Add first three seeds for Historic Events

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Event.destroy_all
 
+Event.create(name: "Fall of the Berlin Wall", date: "November 8, 1989, desc: "Marked the falling of the Iron Curtain and the start of the fall of communism in Eastern and Central Europe", img: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/West_and_East_Germans_at_the_Brandenburg_Gate_in_1989.jpg/480px-West_and_East_Germans_at_the_Brandenburg_Gate_in_1989.jpg")
+Event.create(name: "Apollo 11", date: "July 20, 1969", desc: "Spaceflight that first landed humans on the Moon", img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Aldrin_Apollo_11_original.jpg/520px-Aldrin_Apollo_11_original.jpg")
+Event.create(name: "I Have a Dream Speech", date: "August 28, 1963", desc: "'I Have a Dream' is a public speech that was delivered by American civil rights activist Martin Luther King Jr. during the March on Washington for Jobs and Freedom on August 28, 1963, in which he called for civil and economic rights and an end to racism in the United States.", img= "https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Martin_Luther_King_-_March_on_Washington.jpg/480px-Martin_Luther_King_-_March_on_Washington.jpg")
+
 response = RestClient.get('https://type.fit/api/quotes')
 data = JSON.parse(response)
 
