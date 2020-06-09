899a8d77f41438f46f278bc75d0b1ec4b113e941
hawleywoo <alhawley21@gmail.com>
2020-06-09 12:03:12 -0600

    Add events resource.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Event.destroy_all
+
 response = RestClient.get('https://type.fit/api/quotes')
 data = JSON.parse(response)
 
 data.map do |quote|
     Quote.create(text: quote['text'],author: quote['author'])
 end
+
+
+Event.create(name: '', 
+    date: 'DateTime.strptime("09/01/2009 17:00", "%m/%d/%Y %H:%M")', 
+    desc: '',
+    img: '')
+
